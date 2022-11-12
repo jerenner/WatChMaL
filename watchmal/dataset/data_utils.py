15e3706e51245dd72a2ebc5a8544aa3e61f8d093
KinFit <waleed.a.esmail@gmail.com>
2022-11-12 12:30:28 +0100

    add graph network functionality based on pytorch geometric

--- watchmal/dataset/data_utils.py
+++ watchmal/dataset/data_utils.py
@@ -16,57 +16,51 @@ import random
 # WatChMaL imports
 from watchmal.dataset.samplers import DistributedSamplerWrapper
 
+# pyg imports
+from torch_geometric.loader import DataLoader as PyGDataLoader
 
-def get_data_loader(dataset, batch_size, sampler, num_workers, is_distributed, seed, split_path=None, split_key=None, transforms=None):
+
+def get_data_loader(dataset, is_graph, batch_size, sampler, num_workers, is_distributed, seed, split_path=None, split_key=None, transforms=None):
     """
     Creates a dataloader given the dataset and sampler configs. The dataset and sampler are instantiated using their
     corresponding configs. If using DistributedDataParallel, the sampler is wrapped using DistributedSamplerWrapper.
     A dataloader is returned after being instantiated using this dataset and sampler.
 
+    Args:
+        dataset         ... hydra config specifying dataset object
+        is_graph        ... a boolean indicating whether the dataset is graph or not
+        batch_size      ... batch size
+        sampler         ... hydra config specifying sampler object
+        num_workers     ... number of workers to use in dataloading
+        is_distributed  ... whether running in multiprocessing mode, used to wrap sampler using DistributedSamplerWrapper
+        seed            ... seed used to coordinate samplers in distributed mode
+        split_path      ... path to indices specifying splitting of dataset among train/val/test
+        split_key       ... string key to select indices
+        transforms      ... list of transforms to apply
 
-    Parameters
-    ----------
-    dataset
-        Hydra config specifying dataset object.
-    batch_size : int
-        Size of the batches that the data loader should return.
-    sampler
-        Hydra config specifying sampler object.
-    num_workers : int
-        Number of data loader worker processes to use.
-    is_distributed : bool
-        Whether running in multiprocessing mode (i.e. DistributedDataParallel)
-    seed : int
-        Random seed used to coordinate samplers in distributed mode.
-    split_path
-        Path to an npz file containing an array of indices to use as a subset of the full dataset.
-    split_key : string
-        Name of the array to use in the file specified by split_path.
-    transforms : list of string
-        List of transforms to apply to the dataset.
-    
-    Returns
-    -------
-    torch.utils.data.DataLoader
-        dataloader created with instantiated dataset and (possibly wrapped) sampler
+    Returns: dataloader created with instantiated dataset and (possibly wrapped) sampler
     """
-    dataset = instantiate(dataset, transforms=transforms)
-    
+    dataset = instantiate(dataset, transforms=transforms,
+                          is_distributed=is_distributed)
+
     if split_path is not None and split_key is not None:
         split_indices = np.load(split_path, allow_pickle=True)[split_key]
         sampler = instantiate(sampler, split_indices)
     else:
         sampler = instantiate(sampler)
-    
+
     if is_distributed:
         ngpus = torch.distributed.get_world_size()
 
         batch_size = int(batch_size/ngpus)
-        
+
         sampler = DistributedSamplerWrapper(sampler=sampler, seed=seed)
-    
-    # TODO: added drop_last, should decide if we want to keep this
-    return DataLoader(dataset, sampler=sampler, batch_size=batch_size, num_workers=num_workers, drop_last=False, persistent_workers=True, pin_memory=True)
+
+    if is_graph:
+        return PyGDataLoader(dataset, sampler=sampler, batch_size=batch_size, num_workers=num_workers)
+    else:
+        # TODO: added drop_last, should decide if we want to keep this
+        return DataLoader(dataset, sampler=sampler, batch_size=batch_size, num_workers=num_workers, drop_last=False, persistent_workers=True, pin_memory=True)
 
 
 def get_transformations(transformations, transform_names):
@@ -85,8 +79,10 @@ def get_transformations(transformations, transform_names):
     """
     if transform_names is not None:
         for transform_name in transform_names:
-            assert hasattr(transformations, transform_name), f"Error: There is no defined transform named {transform_name}"
-        transform_funcs = [getattr(transformations, transform_name) for transform_name in transform_names]
+            assert hasattr(
+                transformations, transform_name), f"Error: There is no defined transform named {transform_name}"
+        transform_funcs = [getattr(transformations, transform_name)
+                           for transform_name in transform_names]
         return transform_funcs
     else:
         return None
